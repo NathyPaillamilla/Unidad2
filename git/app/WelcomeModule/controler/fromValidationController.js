0ccb352dc091fd73741d4f636efc7e192e195989
NathyPaillamilla <npaillamilla@yahoo.cl>
2016-05-04 20:14:42 -0300

    Hola

---
+++ git/app/WelcomeModule/controler/fromValidationController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Nathy on 02-05-2016.
+ */
+
+//Generar nuestro modulo
+var app = angular.module('LoginFormModule',[]);
+
+//Controlle
+app.controller('LoginFormController', function($scope,$http) {
+    
+});
