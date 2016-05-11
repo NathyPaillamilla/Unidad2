a7d5efbd274fca0a96d4eb77a82e61ab068f4590
NathyPaillamilla <npaillamilla@yahoo.cl>
2016-05-11 19:54:46 -0300

    algo

--- git/app/WelcomeModule/controler/fromValidationController.js
+++ git/app/WelcomeModule/controler/fromValidationController.js
@@ -8,4 +8,15 @@ var app = angular.module('LoginFormModule',[]);
 //Controlle
 app.controller('LoginFormController', function($scope,$http) {
     
+//Modelo, ntidad generada para sustentar vista
+$scope.formModel ={};
+    
+    //function que llama al Submit.
+    $scope.onSubmit = function () {
+        console.log(" Llegue,! Primer submit con AngularJs");
+        console.log($scope.formModel);
+        //llamadas Http
+        
+    }
+
 });
